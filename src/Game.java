0774dad77a1890bc2d7503290be7dd4267008219
Дмитрий Кузнецов <kuzznec1989@gmail.com>
2023-03-21 11:19:10 +0300

    Добавил класс Player

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,31 @@
 
-
 public class Game {
+    private final String[][] gameField = new String[3][3];
+    public static final String X = "|X|";
+    public static final String O = "|O|";
+    public static final String EMPTY = "|_|";
+    public Game(){
+        init();
+    }
+    public void init(){
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField.length; j++) {
+                gameField[i][j] = EMPTY;
+            }
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        for(String[] field: gameField){
+            for(String field1: field){
+                System.out.print(field1);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        return "";
+    }
 
 }
---
+++ src/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+
+    String[][] gameField;
+    String symbol;
+
+    public Player(String[][] gameField, String symbol) {
+        this.gameField = gameField;
+        this.symbol = symbol;
+    }
+
+    void makeStep(){}
+}
