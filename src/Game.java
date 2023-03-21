2b12fb3c9c75ba4ccc55a460ae36f66f578133c5
Дмитрий Кузнецов <kuzznec1989@gmail.com>
2023-03-21 16:09:12 +0300

    В клас Game добавил 2 метода. Один из них ищет победителя, а второй ничью

--- src/Game.java
+++ src/Game.java
@@ -16,6 +16,35 @@ public class Game {
         }
     }
 
+    public boolean checkWinner(){
+        for (int i = 0; i < gameField.length; i++) {
+            if(gameField[i][0].equals(X) && gameField[i][1].equals(X) && gameField[i][2].equals(X) ||
+               gameField[0][i].equals(X) && gameField[1][i].equals(X) && gameField[2][i].equals(X) ||
+               gameField[0][0].equals(X) && gameField[1][1].equals(X) && gameField[2][2].equals(X) ||
+               gameField[2][0].equals(X) && gameField[1][1].equals(X) && gameField[0][2].equals(X)){
+                System.out.println("Победа крестиков");
+                return true;
+            } else if (gameField[i][0].equals(O) && gameField[i][1].equals(O) && gameField[i][2].equals(O) ||
+                       gameField[0][i].equals(O) && gameField[1][i].equals(O) && gameField[2][i].equals(O) ||
+                       gameField[0][0].equals(O) && gameField[1][1].equals(O) && gameField[2][2].equals(O) ||
+                       gameField[2][0].equals(O) && gameField[1][1].equals(O) && gameField[0][2].equals(O)) {
+                System.out.println("Победа ноликов");
+            }
+        }
+        return false;
+    }
+    public boolean checkDraw(){
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField.length; j++) {
+                if(gameField[i][j].equals(EMPTY)){
+                    return false;
+                }
+            }
+        }
+        System.out.println("Ничья");
+        return true;
+    }
+
     @Override
     public String toString() {
         for(String[] field: gameField){
